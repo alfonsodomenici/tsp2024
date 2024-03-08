c6aa03d864c49866fe6dca9a77d5d4f056269c87
Alfonso Domenici <alfonso.domenici@gmail.com>
2024-03-08 17:27:30 +0100

    esercizio ecommerce

--- 01-esercizi-progr-base/src/oo/ecommerce/App.java
+++ 01-esercizi-progr-base/src/oo/ecommerce/App.java
@@ -1,20 +1,32 @@
 package oo.ecommerce;
 
+import java.util.List;
+import java.util.Set;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("-------------- Start ecommerce test --------------");
 
         Product p = new Product("caffe", 12);
 
-        System.out.println(p);
+        Storage storage = new Storage();
 
-        p.setName("zucchero");
-        p.setPrice(2);
+        storage.load(new Product("caffe vergnano", 2), 100);
+        storage.load(new Product("caffe lavazza", 3), 10);
+        storage.load(new Product("caffe Illy", 4), 30);
+        storage.load(new Product("spagetti", 2.5f), 200);
+        storage.load(new Product("latte", 3), 12);
+        storage.load(new Product("birra", 4.5f), 45);
+        storage.load(new Product("birra", 4.8f), 60);
 
-        System.out.println(p);
+        storage.logStatus();
 
-        p.setName(null);
+        List<Product> foundProducts = storage.findByNameAndAvailabilities("caffe",5);
 
-        System.out.println(p);
+        foundProducts.forEach(v -> v.setPrice(v.getPrice() * 1000));
+
+        storage.logStatus();
+        
+
     }
 }
--- 01-esercizi-progr-base/src/oo/ecommerce/Storage.java
+++ 01-esercizi-progr-base/src/oo/ecommerce/Storage.java
@@ -1,13 +1,24 @@
 package oo.ecommerce;
 
 import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
+/*
+ * 
+ * 
+ * 
+ */
 public class Storage {
 
     private Set<StorageItem> items = new HashSet<>();
 
+    /*
+     * ------------------------ public methods ------------------------------------
+     */
     /**
      * carica un prodotto a magazzino
      * 
@@ -19,30 +30,48 @@ public class Storage {
                 .orElse(new StorageItem(product, 0));
         item.setQuantity(item.getQuantity() + quantity);
         items.add(item);
+
     }
 
+    /*
+     * scarica un prodotto dal magazzino
+     * 
+     */
     public void unload(Product product, int quantity) {
         StorageItem item = isAvailable(product, quantity);
         item.setQuantity(item.getQuantity() - quantity);
     }
 
-    /*
-     * ritorna la disponibilità oppure errore
-     * 
-     */
-    public StorageItem isAvailable(Product product, int quantity) {
-        StorageItem found = find(product).orElseThrow(() -> new StorageException("product not exist"));
-        if (found.getQuantity() < quantity) {
-            throw new StorageException("quantity in not sufficient");
-        }
-        return found;
-    }
-
     public void discontinue(Product p) {
-        StorageItem found = find(p).orElseThrow(() -> new StorageException("product not exist"));
+        StorageItem found = find(p)
+                .orElseThrow(() -> new StorageException("product not exist"));
         found.discontinue();
     }
 
+    public List<Product> findByName(String name) {
+        return items.stream()
+                .filter(v -> v.getProduct().getName().contains(name))
+                .map(v -> v.getProduct())
+                .collect(Collectors.toList());
+    }
+
+    public List<Product> findByNameAndAvailabilities(String name, int quantity) {
+        return items.stream()
+                .filter(v -> v.getProduct().getName().contains(name) && v.getQuantity() > quantity)
+                .map(v -> v.getProduct())
+                .collect(Collectors.toList());
+    }
+
+    public void logStatus() {
+        System.out.println("-------------STORAGE STATUS ---------------------");
+        items.forEach(v -> System.out.println(v));
+        System.out.println("-------------STORAGE STATUS ---------------------");
+    }
+
+    /*
+     * -------------------------------- private meyhods
+     * ------------------------------------
+     */
     /*
      * ricerca il prodotto in magazzino
      */
@@ -50,6 +79,18 @@ public class Storage {
         return items.stream()
                 .filter(v -> v.getProduct().equals(p))
                 .findFirst();
+    }
+
+    /*
+     * ritorna la disponibilità oppure errore
+     * 
+     */
+    private StorageItem isAvailable(Product product, int quantity) {
+        StorageItem found = find(product).orElseThrow(() -> new StorageException("product not exist"));
+        if (found.getQuantity() < quantity) {
+            throw new StorageException("quantity in not sufficient");
+        }
+        return found;
     }
 
     /*
@@ -82,4 +123,75 @@ public class Storage {
 
     }
 
+    private class StorageItem {
+
+        public static enum State {
+            IN_STOCK, OUT_OF_STOCK, DISCONTINUED
+        }
+
+        private Product product;
+        private int quantity;
+        private State state;
+
+        public StorageItem(Product product, int quantity) {
+            Objects.requireNonNull(product, "empty product not allowed");
+            this.product = product;
+            setQuantity(quantity);
+        }
+
+        public Product getProduct() {
+            return product;
+        }
+
+        public State getState() {
+            return state;
+        }
+
+        public int getQuantity() {
+            return quantity;
+        }
+
+        public void setQuantity(int quantity) {
+            if (quantity < 0) {
+                throw new IllegalArgumentException("negative quantity not allowed");
+            }
+            this.quantity = quantity;
+            this.state = this.quantity > 0 ? State.IN_STOCK : State.OUT_OF_STOCK;
+        }
+
+        public void discontinue() {
+            this.state = State.DISCONTINUED;
+        }
+
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + ((product == null) ? 0 : product.hashCode());
+            return result;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            StorageItem other = (StorageItem) obj;
+            if (product == null) {
+                if (other.product != null)
+                    return false;
+            } else if (!product.equals(other.product))
+                return false;
+            return true;
+        }
+
+        @Override
+        public String toString() {
+            return "StorageItem [product=" + product + ", quantity=" + quantity + ", state=" + state + "]";
+        }
+
+    }
 }
